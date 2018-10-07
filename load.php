5b64d5a49f1dd73c8eac94a8eb5fa230e104c809
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 19:00:22 +0500

    .

--- load.php
+++ load.php
@@ -3,5 +3,16 @@
 if (!defined('ABSPATH')) {
   define('ABSPATH', dirname(__FILE__).'/');
 }
-define('APPINC', 'app-includes');
+
+  define('APPINC', 'app-includes');
+
+  include 'tests/appconnect.php';
+  include 'tests/usertable.php';
+  include 'tests/product_types.php';
+  include 'tests/product_brands.php';
+  include 'tests/vendors.php';
+  include 'tests/expense_accounts.php';
+  include 'tests/products.php';
+  include 'tests/sales.php';
+
 ?>
