1a855551c46e87a84f68d3ab57dd5add1e9c72ab
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 13:16:04 +0500

    using products-table.php

--- products.php
+++ products.php
@@ -10,155 +10,32 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php require (ABSPATH.APPINC."/page-unit/head.php"); ?>
+  <?php require (ABSPATH.APPINC."/page-unit/head.php"); ?>
   <body id="page-top">
-<?php require (ABSPATH.APPINC."/page-unit/nav.php"); ?>
+    <?php require (ABSPATH.APPINC."/page-unit/nav.php"); ?>
     <div id="wrapper">
-<?php require (ABSPATH.APPINC."/page-unit/sidebar.php"); ?>
-        <div id="content-wrapper">
-          <div class="container-fluid">
-<?php require (ABSPATH.APPINC."/page-unit/breadcrumbs.php"); ?>
-            <!-- Page Content -->
-            <!-- DataTables Example -->
-            <div class="card mb-3">
-              <div class="card-header bg-primary text-white">
-                <i class="fa fa-table"></i>
-                Products
-                <a href="#" class="text-white" data-toggle="modal" data-target="#addProductModal">
-                  <span class="float-right">
-                    <i class="fa fa-plus"></i>
-                    Add New Product
-                  </span>
-                </a>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive">
-                  <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                      <tr>
-                        <th>PID</th>
-                        <th>Product Name</th>
-                        <th>Product Type</th>
-                        <th>Brand</th>
-                        <th>In-stock</th>
-                        <th>Cost/item</th>
-                        <th>Inventory Worth</th>
-                        <th>Revenue Generated</th>
-                        <th>Vendor</th>
-                      </tr>
-                    </thead>
-                    <tfoot>
-                      <tr>
-                        <th>PID</th>
-                        <th>Product Name</th>
-                        <th>Product Type</th>
-                        <th>Brand</th>
-                        <th>In-stock</th>
-                        <th>Cost/item</th>
-                        <th>Inventory Worth</th>
-                        <th>Revenue Generated</th>
-                        <th>Vendor</th>
-                      </tr>
-                    </tfoot>
-                    <tbody>
-                      <tr>
-                        <td>054681</td>
-                        <td>A4-Tech Mouse M60</td>
-                        <td>Mouse &amp; Pointing Devices</td>
-                        <td>A4-Tech</td>
-                        <td>6</td>
-                        <td>Rs840</td>
-                        <td>Rs5040</td>
-                        <td>Rs1370</td>
-                        <td>Anees Ahmad</td>
-                      </tr>
-                      <tr>
-                        <td>054681</td>
-                        <td>A4-Tech Mouse M60</td>
-                        <td>Mouse &amp; Pointing Devices</td>
-                        <td>A4-Tech</td>
-                        <td>6</td>
-                        <td>Rs840</td>
-                        <td>Rs5040</td>
-                        <td>Rs1370</td>
-                        <td>Anees Ahmad</td>
-                      </tr>
-                      <tr>
-                        <td>054681</td>
-                        <td>A4-Tech Mouse M60</td>
-                        <td>Mouse &amp; Pointing Devices</td>
-                        <td>A4-Tech</td>
-                        <td>6</td>
-                        <td>Rs840</td>
-                        <td>Rs5040</td>
-                        <td>Rs1370</td>
-                        <td>Anees Ahmad</td>
-                      </tr>
-                      <tr>
-                        <td>054681</td>
-                        <td>A4-Tech Mouse M60</td>
-                        <td>Mouse &amp; Pointing Devices</td>
-                        <td>A4-Tech</td>
-                        <td>6</td>
-                        <td>Rs840</td>
-                        <td>Rs5040</td>
-                        <td>Rs1370</td>
-                        <td>Anees Ahmad</td>
-                      </tr>
-                      <tr>
-                        <td>054681</td>
-                        <td>A4-Tech Mouse M60</td>
-                        <td>Mouse &amp; Pointing Devices</td>
-                        <td>A4-Tech</td>
-                        <td>6</td>
-                        <td>Rs840</td>
-                        <td>Rs5040</td>
-                        <td>Rs1370</td>
-                        <td>Anees Ahmad</td>
-                      </tr>
-                      <tr>
-                        <td>054681</td>
-                        <td>A4-Tech Mouse M60</td>
-                        <td>Mouse &amp; Pointing Devices</td>
-                        <td>A4-Tech</td>
-                        <td>6</td>
-                        <td>Rs840</td>
-                        <td>Rs5040</td>
-                        <td>Rs1370</td>
-                        <td>Anees Ahmad</td>
-                      </tr>
-                      <tr>
-                        <td>054681</td>
-                        <td>A4-Tech Mouse M60</td>
-                        <td>Mouse &amp; Pointing Devices</td>
-                        <td>A4-Tech</td>
-                        <td>6</td>
-                        <td>Rs840</td>
-                        <td>Rs5040</td>
-                        <td>Rs1370</td>
-                        <td>Anees Ahmad</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-              <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-            </div>
+      <?php require (ABSPATH.APPINC."/page-unit/sidebar.php"); ?>
+      <div id="content-wrapper">
+        <div class="container-fluid">
+          <?php
+            require (ABSPATH.APPINC."/page-unit/breadcrumbs.php");
+            require (ABSPATH.APPINC."/tables/products-table.php");
+            ?>
           </div>
           <br><br><br>
           <?php require (ABSPATH.APPINC.'/page-unit/footer.php'); ?>
         </div>
       </div>
-  <?php
-    include (ABSPATH.APPINC.'/page-unit/scroll-to-top.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-logout.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-sale.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-product.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-product-type.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-brand.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-vendor.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-expense-account.php');
-    include (ABSPATH.APPINC."/page-unit/footer-scripts.php");
-  ?>
+      <?php
+        include (ABSPATH.APPINC.'/page-unit/scroll-to-top.php');
+        require (ABSPATH.APPINC.'/page-unit/modal-logout.php');
+        require (ABSPATH.APPINC.'/page-unit/modal-add-sale.php');
+        require (ABSPATH.APPINC.'/page-unit/modal-add-product.php');
+        require (ABSPATH.APPINC.'/page-unit/modal-add-product-type.php');
+        require (ABSPATH.APPINC.'/page-unit/modal-add-brand.php');
+        require (ABSPATH.APPINC.'/page-unit/modal-add-vendor.php');
+        require (ABSPATH.APPINC.'/page-unit/modal-add-expense-account.php');
+        include (ABSPATH.APPINC."/page-unit/footer-scripts.php");
+      ?>
     </body>
   </html>
