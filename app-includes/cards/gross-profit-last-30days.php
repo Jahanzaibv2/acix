7cec470c23f1e479772a6526c75a53526e4ccc0b
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 14:11:58 +0500

    using demo-last-6months-profit-chart.php

---
+++ app-includes/cards/gross-profit-last-30days.php
@@ -0,0 +1,20 @@
+<?php  ?>
+<div class="card text-white bg-info o-hidden h-100">
+  <div class="card-header">
+    <h1>Last 30 Days</h1>
+  </div>
+  <div class="card-body">
+    <div class="card-body-icon">
+      <i class="fa fa-fw fa-money"></i>
+    </div>
+    <div class="card-text">
+      <h1 class="text-center display-3"><strong>Rs38.0k</strong></h1>
+    </div>
+  </div>
+  <a class="card-footer text-white clearfix small z-1" href="">
+    <span class="float-left">View Details</span>
+    <span class="float-right">
+      <i class="fa fa-angle-right"></i>
+    </span>
+  </a>
+</div>
---
+++ app-includes/charts/demo-last-6months-profit-area-chart.php
@@ -0,0 +1,11 @@
+<!-- Last 6 Months Profit Area CHart-->
+<div class="card mb-3">
+  <div class="card-header bg-primary text-white">
+    <i class="fa fa-chart-area"></i>
+    Last 6 Months Profit
+  </div>
+  <div class="card-body">
+    <canvas id="myAreaChart" width="100%" height="30"></canvas>
+  </div>
+  <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+</div>
