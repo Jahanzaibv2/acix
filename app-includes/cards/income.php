16ead0b3f807c114cfc207fbe93becf133e0bbb4
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 12:55:52 +0500

    using cards/ tables/

---
+++ app-includes/cards/income.php
@@ -0,0 +1,20 @@
+<?php  ?>
+<div class="card o-hidden h-100">
+  <div class="card-header bg-primary text-white">
+    <h1>Income</h1>
+  </div>
+  <div class="card-body">
+    <div class="card-body-icon text-white">
+      <i class="fa fa-fw fa-shopping-cart"></i>
+    </div>
+    <div class="card-text text-center">
+      <span class="display-3"><strong>38.2</strong>k</span>
+    </div>
+  </div>
+  <a class="card-footer clearfix small z-1" href="#">
+    <span class="float-left">View Details</span>
+    <span class="float-right">
+      <i class="fa fa-angle-right"></i>
+    </span>
+  </a>
+</div>
---
+++ app-includes/tables/payment-reports-table.php
@@ -0,0 +1,187 @@
+<?php  ?>
+<!-- Payment Reports Table -->
+<div class="card mb-3">
+  <div class="card-header bg-primary text-white">
+    <i class="fa fa-table"></i>
+    Payment Reports
+    <a href="#" class="text-white" data-toggle="modal" data-target="#addSaleModal">
+      <span class="float-right">
+        <i class="fa fa-plus"></i>
+        Add Expense
+      </span>
+    </a>
+  </div>
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th>TID</th>
+            <th>Transaction Type</th>
+            <th>Description</th>
+            <th>Account</th>
+            <th>Date</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th>TID</th>
+            <th>Transaction Type</th>
+            <th>Description</th>
+            <th>Account</th>
+            <th>Date</th>
+            <th>Price</th>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr>
+            <td>#456421</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 2 Fresh Juices</td>
+            <td>Akhtar Hotel</td>
+            <td>03/10/2018</td>
+            <td>Rs120</td>
+          </tr>
+          <tr>
+            <td>#456519</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#456517</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#456515</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#455314</td>
+            <td class="text-primary">Income</td>
+            <td>Audionic MAX-4 Sold out</td>
+            <td>Cash Register</td>
+            <td>04/10/2018</td>
+            <td>Rs2350</td>
+          </tr>
+          <tr>
+            <td>#456513</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#455312</td>
+            <td class="text-primary">Income</td>
+            <td>Audionic MAX-4 Sold out</td>
+            <td>Cash Register</td>
+            <td>04/10/2018</td>
+            <td>Rs2350</td>
+          </tr>
+          <tr>
+            <td>#456511</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#455310</td>
+            <td class="text-primary">Income</td>
+            <td>Audionic MAX-4 Sold out</td>
+            <td>Cash Register</td>
+            <td>04/10/2018</td>
+            <td>Rs2350</td>
+          </tr>
+          <tr>
+            <td>#456509</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#455308</td>
+            <td class="text-primary">Income</td>
+            <td>Audionic MAX-4 Sold out</td>
+            <td>Cash Register</td>
+            <td>04/10/2018</td>
+            <td>Rs2350</td>
+          </tr>
+          <tr>
+            <td>#456507</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#455306</td>
+            <td class="text-primary">Income</td>
+            <td>Audionic MAX-4 Sold out</td>
+            <td>Cash Register</td>
+            <td>04/10/2018</td>
+            <td>Rs2350</td>
+          </tr>
+          <tr>
+            <td>#456505</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#455304</td>
+            <td class="text-primary">Income</td>
+            <td>Audionic MAX-4 Sold out</td>
+            <td>Cash Register</td>
+            <td>04/10/2018</td>
+            <td>Rs2350</td>
+          </tr>
+          <tr>
+            <td>#456503</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+          <tr>
+            <td>#455302</td>
+            <td class="text-primary">Income</td>
+            <td>Audionic MAX-4 Sold out</td>
+            <td>Cash Register</td>
+            <td>04/10/2018</td>
+            <td>Rs2350</td>
+          </tr>
+          <tr>
+            <td>#456501</td>
+            <td class="text-danger">Expense</td>
+            <td>Served 4 Teas</td>
+            <td>Mian Tea Stall</td>
+            <td>03/10/2018</td>
+            <td>Rs80</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="card-footer small text-muted">This table shows last 100 Enteries. Check all other enteries <a href="#">here</a>.</div>
+</div>
