e310dd02a079fbd2b85c376cfb1d9e632239587e
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 13:53:01 +0500

    using demo-improvement-*-chart.php

---
+++ app-includes/charts/demo-improvement-pie-chart.php
@@ -0,0 +1,11 @@
+<?php  ?>
+<div class="card mb-3">
+  <div class="card-header bg-dark text-white">
+    <i class="fa fa-chart-pie"></i>
+    Product Profits Chart
+  </div>
+  <div class="card-body">
+    <canvas id="myPieChart" width="100%" height="100"></canvas>
+  </div>
+  <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+</div>
--- improvements.php
+++ improvements.php
@@ -10,56 +10,37 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php require (ABSPATH.APPINC."/page-unit/head.php"); ?>
+  <?php require (ABSPATH.APPINC."/page-unit/head.php"); ?>
   <body id="page-top">
-<?php require (ABSPATH.APPINC."/page-unit/nav.php"); ?>
+    <?php require (ABSPATH.APPINC."/page-unit/nav.php"); ?>
     <div id="wrapper">
-<?php require (ABSPATH.APPINC."/page-unit/sidebar.php"); ?>
-        <div id="content-wrapper">
-          <div class="container-fluid">
-<?php require (ABSPATH.APPINC."/page-unit/breadcrumbs.php"); ?>
-            <!-- Page Content -->
-            <div class="row">
+      <?php require (ABSPATH.APPINC."/page-unit/sidebar.php"); ?>
+      <div id="content-wrapper">
+        <div class="container-fluid">
+          <?php require (ABSPATH.APPINC."/page-unit/breadcrumbs.php"); ?>
+          <div class="row">
             <div class="col-lg-8 col-sm-12 col-md-12 col-xs-12">
-              <div class="card mb-3">
-                <div class="card-header bg-primary text-white">
-                  <i class="fa fa-chart-bar"></i>
-                  Improvements Chart
-                </div>
-                <div class="card-body">
-                  <canvas id="myBarChart" width="100%" height="50"></canvas>
-                </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-              </div>
+              <?php require (ABSPATH.APPINC."/charts/demo-improvement-bar-chart.php"); ?>
             </div>
             <div class="col-lg-4">
-              <div class="card mb-3">
-                <div class="card-header bg-dark text-white">
-                  <i class="fa fa-chart-pie"></i>
-                  Product Profits Chart
-                </div>
-                <div class="card-body">
-                  <canvas id="myPieChart" width="100%" height="100"></canvas>
-                </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-              </div>
-            </div>
+              <?php require (ABSPATH.APPINC."/charts/demo-improvement-pie-chart.php"); ?>
             </div>
           </div>
-          <br><br><br>
-          <?php require (ABSPATH.APPINC.'/page-unit/footer.php'); ?>
         </div>
+        <br><br><br>
+        <?php require (ABSPATH.APPINC.'/page-unit/footer.php'); ?>
       </div>
-  <?php
-    include (ABSPATH.APPINC.'/page-unit/scroll-to-top.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-logout.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-sale.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-product.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-product-type.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-brand.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-vendor.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-expense-account.php');
-    include (ABSPATH.APPINC."/page-unit/footer-scripts.php");
-  ?>
+    </div>
+    <?php
+      include (ABSPATH.APPINC.'/page-unit/scroll-to-top.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-logout.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-sale.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-product.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-product-type.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-brand.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-vendor.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-expense-account.php');
+      include (ABSPATH.APPINC."/page-unit/footer-scripts.php");
+    ?>
     </body>
   </html>
