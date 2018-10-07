1397dddb76b784bdab2c6ae8e15b3ffdfb3c6b63
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 10:08:02 +0500

    require load.php;

--- forgot-password.php
+++ forgot-password.php
@@ -1,10 +1,6 @@
 <?php
-// Define ABSPATH as this file's directory
-if (!defined('ABSPATH')) {
-  define('ABSPATH', dirname(__FILE__).'/');
-}
 
-define('APPINC', 'app-includes');
+  require ('load.php');
 
 
   $PAGE_TITLE  = "Request a New Password";
---
+++ load.php
@@ -0,0 +1,7 @@
+<?php
+// Define ABSPATH as this file's directory
+if (!defined('ABSPATH')) {
+  define('ABSPATH', dirname(__FILE__).'/');
+}
+define('APPINC', 'app-includes');
+?>
