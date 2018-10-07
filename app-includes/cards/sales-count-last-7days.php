b4417d1598b1f6ed6d05e8b8355ad68dd7166db8
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 14:23:50 +0500

    restructured sale-count.php

---
+++ app-includes/cards/sales-count-last-7days.php
@@ -0,0 +1,20 @@
+<?php  ?>
+<div class="card text-white bg-primary o-hidden h-100">
+  <div class="card-header">
+    <h1>Last 7 Days</h1>
+  </div>
+  <div class="card-body">
+    <div class="card-body-icon">
+      <i class="fa fa-fw fa-money"></i>
+    </div>
+    <div class="card-text">
+      <h1 class="text-center display-3"><strong>135<span class="small" style="font-size: 20px;">Rs9.52k</span></strong></h1>
+    </div>
+  </div>
+  <a class="card-footer text-white clearfix small z-1" href="sales-count.php">
+    <span class="float-left">View Details</span>
+    <span class="float-right">
+      <i class="fa fa-angle-right"></i>
+    </span>
+  </a>
+</div>
