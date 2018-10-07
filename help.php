fde2690f35a2273014dd2657937f4dd0350548dc
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 16:21:12 +0500

    indent

--- help.php
+++ help.php
@@ -10,33 +10,32 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php require (ABSPATH.APPINC."/page-unit/head.php"); ?>
+  <?php require (ABSPATH.APPINC."/page-unit/head.php"); ?>
   <body id="page-top">
-<?php require (ABSPATH.APPINC."/page-unit/nav.php"); ?>
+    <?php require (ABSPATH.APPINC."/page-unit/nav.php"); ?>
     <div id="wrapper">
-<?php require (ABSPATH.APPINC."/page-unit/sidebar.php"); ?>
-        <div id="content-wrapper">
-          <div class="container-fluid">
-<?php require (ABSPATH.APPINC."/page-unit/breadcrumbs.php"); ?>
-            <!-- Page Content -->
-            <h1>Help Docs</h1>
-            <hr>
-            <p>This is a great starting point for new custom pages.</p>
-          </div>
-          <br><br><br>
-          <?php require (ABSPATH.APPINC.'/page-unit/footer.php'); ?>
+      <?php require (ABSPATH.APPINC."/page-unit/sidebar.php"); ?>
+      <div id="content-wrapper">
+        <div class="container-fluid">
+          <?php require (ABSPATH.APPINC."/page-unit/breadcrumbs.php"); ?>
+          <h1>Help Docs</h1>
+          <hr>
+          <p>This is a great starting point for new custom pages.</p>
         </div>
+        <br><br><br>
+        <?php require (ABSPATH.APPINC.'/page-unit/footer.php'); ?>
       </div>
-  <?php
-    include (ABSPATH.APPINC.'/page-unit/scroll-to-top.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-logout.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-sale.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-product.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-product-type.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-brand.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-vendor.php');
-    require (ABSPATH.APPINC.'/page-unit/modal-add-expense-account.php');
-    include (ABSPATH.APPINC."/page-unit/footer-scripts.php");
-  ?>
+    </div>
+    <?php
+      include (ABSPATH.APPINC.'/page-unit/scroll-to-top.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-logout.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-sale.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-product.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-product-type.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-brand.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-vendor.php');
+      require (ABSPATH.APPINC.'/page-unit/modal-add-expense-account.php');
+      include (ABSPATH.APPINC."/page-unit/footer-scripts.php");
+    ?>
     </body>
   </html>
