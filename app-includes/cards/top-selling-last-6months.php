62fdc6b2502dcfce58caa2e866233871149da04d
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 14:32:29 +0500

    restructured top-selling.php

---
+++ app-includes/cards/top-selling-last-6months.php
@@ -0,0 +1,11 @@
+<?php  ?>
+<div class="card mb-3">
+  <div class="card-header bg-dark text-white">
+    <i class="fa fa-chart-pie"></i>
+    Top Selling Products Last 6 Months
+  </div>
+  <div class="card-body">
+    <canvas id="myPieChart6Months" width="100%" height="100"></canvas>
+  </div>
+  <div class="card-footer small text-muted">Updated 2s ago</div>
+</div>
