2dbbb67c3ffd84c06b6c061774fd0137c9680545
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-07 11:24:52 +0500

    using tables and cards/

---
+++ app-includes/cards/backup.php
@@ -0,0 +1,16 @@
+<?php  ?>
+<div class="col-xl-6 col-sm-12 mb-3">
+  <a href="#" class="no-text-decorations">
+    <div class="card o-hidden h-100">
+      <div class="card-body bg-dark text-white">
+        <div class="card-body-icon">
+          <i class="fa fa-fw fa-copy text-white"></i>
+        </div>
+        <div class="card-text text-center">
+          <h1 class="display-1"><strong>Backup</strong></h1>
+          Create custom backups of your data
+        </div>
+      </div>
+    </div>
+  </a>
+  </div>
---
+++ app-includes/cards/clone.php
@@ -0,0 +1,16 @@
+<?php  ?>
+<div class="col-xl-6 col-sm-12 mb-3">
+  <a href="#" class="no-text-decorations">
+    <div class="card o-hidden h-100">
+      <div class="card-body bg-info text-white">
+        <div class="card-body-icon">
+          <i class="fa fa-fw fa-clone text-white"></i>
+        </div>
+        <div class="card-text text-center">
+          <h1 class="display-1"><strong>Clone</strong></h1>
+          Extract & Run ./<strong>getinstalled.sh</strong> to bring system live
+        </div>
+      </div>
+    </div>
+  </a>
+  </div>
