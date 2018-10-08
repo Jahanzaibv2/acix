261856d9da1dd0062184c08874de84e2520004a1
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-08 20:03:29 +0500

    add-product

--- app-includes/scripts/add-product.php
+++ app-includes/scripts/add-product.php
@@ -9,6 +9,8 @@
     $paymentOption = mysqli_real_escape_string($appconnect, $_POST['payment_option']);
     $productDesc = mysqli_real_escape_string($appconnect, $_POST['product_desc']);
 
+    $thisMonth = date('FY');
+
 
     $res = mysqli_query($appconnect, "SELECT * FROM `products` WHERE `name`='$productName'");
     $row = mysqli_fetch_array($res);
@@ -43,6 +45,54 @@
                                               current_timestamp(),
                                               NULL)"
                                             );
+
+      // Verifies if data is inserted and deducts amount from Store Account if requested
+      $res = mysqli_query($appconnect, "SELECT * FROM `products` WHERE `name`='$productName'");
+      if ((mysqli_num_rows($res))>0) {
+        if ($paymentOption == "store_account") {
+          $res = mysqli_query($appconnect, "SELECT * FROM `store_account` WHERE `month`='$thisMonth'");
+          if ((mysqli_num_rows($res)) != 1) {
+            // Creates record if not exists. This query helps creating new table data for new month
+            mysqli_query($appconnect, "INSERT INTO `store_account` (
+                                                    `id`,
+                                                    `balance`,
+                                                    `month`,
+                                                    `income`,
+                                                    `expense`,
+                                                    `date_created`
+                                                  ) VALUES (
+                                                    NULL,
+                                                    '0',
+                                                    '$thisMonth',
+                                                    '0',
+                                                    '0',
+                                                    current_timestamp())"
+                                                  );
+          }
+
+          // Fetches data for current month
+          $res = mysqli_query($appconnect, "SELECT * FROM `store_account` WHERE `month`='$thisMonth'");
+          $row = mysqli_fetch_array($res);
+          $balance = $row['balance'];
+          $income = $row['income'];
+          $expense = $row['expense'];
+
+          // Sets updated data
+          $newBalance = $balance - ($productPrice * $sku);
+          $newExpense = $expense + ($productPrice * $sku);
+
+          // Updates data for current month
+          mysqli_query($appconnect, "UPDATE `store_account` SET
+                                             `balance` = '$newBalance',
+                                             `income` = '$income',
+                                             `expense` = '$newExpense'
+                                             WHERE `store_account`.`month` = '$thisMonth'"
+                                           );
+
+        }else {
+          echo 'Radio Error';
+        }
+      }
     }
   }
 ?>
