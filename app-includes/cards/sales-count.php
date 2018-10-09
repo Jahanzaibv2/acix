f615b7e966fec80935a259792fea1d7f00b74ba6
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-09 20:48:47 +0500

    get sales count

--- app-includes/cards/sales-count.php
+++ app-includes/cards/sales-count.php
@@ -1,15 +1,25 @@
-<?php  ?>
+<?php
+
+
+  $thisMonth = date('FY');
+  $prevMonth = date('FY', strtotime("last month"));
+
+  $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$thisMonth'");
+  $salesCount = mysqli_num_rows($res);
+
+?>
+
                 <div class="card text-white bg-primary o-hidden h-100">
                   <div class="card-header">
                     <h1>Sales Count</h1>
-                    <small class="float-left">Last two weeks</small>
+                    <small class="float-left">This month</small>
                   </div>
                   <div class="card-body">
                     <div class="card-body-icon">
                       <i class="fa fa-fw fa-money"></i>
                     </div>
                     <div class="card-text">
-                      <h1 class="text-center display-3"><strong>135</strong></h1>
+                      <h1 class="text-center display-3"><strong><?php echo $salesCount; ?></strong></h1>
                     </div>
                   </div>
                   <a class="card-footer text-white clearfix small z-1" href="sales-count.php">
