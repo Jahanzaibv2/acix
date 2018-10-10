0e689cc4db51b7e96285a88c25533516619f7c43
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-10 08:55:52 +0500

    .

--- app-includes/cards/backup.php
+++ app-includes/cards/backup.php
@@ -1,14 +1,19 @@
-<?php  ?>
+<?php
+
+?>
   <a href="#" class="no-text-decorations">
+    <a class="no-text-decorations text-white" href="<?php echo ('../admin/scripts/export-record.php'); ?>" download>
     <div class="card o-hidden h-100">
       <div class="card-body bg-dark text-white">
         <div class="card-body-icon">
           <i class="fa fa-fw fa-copy text-white"></i>
         </div>
-        <div class="card-text text-center">
-          <h1 class="display-1"><strong>Backup</strong></h1>
-          Create custom backups of your data
-        </div>
+
+          <div class="card-text text-center">
+            <h1 class="display-1"><strong>Backup</strong></h1>
+            Download & keep your backup file safe
+          </div>
       </div>
     </div>
+  </a>
   </a>
