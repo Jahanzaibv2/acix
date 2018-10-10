9ec418a5d2c56fb5420dac478a6d516a3c118e00
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-10 08:39:56 +0500

    .

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,55 @@
-<?php  ?>
+<?php
+
+  header('location: /');
+
+/*
+
+
+  ////////////////////////////////////////////////
+  /
+  / You can use it for additional admin settings
+  / for now I'm leaving this blank
+  /
+  ///////////////////////////////////////////////
+
+
+  require ('../load.php');
+
+
+  $PAGE_TITLE  = "General Settings";
+  $PAGE_DESC   = "...";
+  $PAGE_AUTHOR = "..."
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <?php require (ABSPATH.APPINC."/modules/head.php"); ?>
+  <body id="page-top">
+    <?php require (ABSPATH.APPINC."/modules/nav.php"); ?>
+    <div id="wrapper">
+      <?php require (ABSPATH.APPINC."/modules/sidebar.php"); ?>
+      <div id="content-wrapper">
+        <div class="container-fluid">
+          <?php require (ABSPATH.APPINC."/modules/breadcrumbs.php"); ?>
+          <h1>General Settings</h1>
+          <hr>
+          <p>This is a great starting point for new custom pages.</p>
+        </div>
+        <br><br><br>
+        <?php require (ABSPATH.APPINC.'/modules/footer.php'); ?>
+      </div>
+    </div>
+    <?php
+      include (ABSPATH.APPINC.'/modules/scroll-to-top.php');
+      require (ABSPATH.APPINC.'/modules/modal-logout.php');
+      require (ABSPATH.APPINC.'/modules/modal-add-sale.php');
+      require (ABSPATH.APPINC.'/modules/modal-add-product.php');
+      require (ABSPATH.APPINC.'/modules/modal-add-product-type.php');
+      require (ABSPATH.APPINC.'/modules/modal-add-brand.php');
+      require (ABSPATH.APPINC.'/modules/modal-add-vendor.php');
+      require (ABSPATH.APPINC.'/modules/modal-add-expense-account.php');
+      include (ABSPATH.APPINC."/modules/footer-scripts.php");
+    ?>
+    </body>
+  </html>
+*/
