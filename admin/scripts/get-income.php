f58b37c424fb26ab3a6903f5fe43625289c611d9
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-11 22:14:22 +0500

    get income

--- admin/scripts/get-income.php
+++ admin/scripts/get-income.php
@@ -5,7 +5,7 @@ session_start();
 
   if (isset($_SESSION['user_id'])) {
 
-  $res = mysqli_query($appconnect, "SELECT * FROM `store_account` WHERE `month`<='$thisMonth' AND `year`='$thisYear'");
+  $res = mysqli_query($appconnect, "SELECT * FROM `store_account` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
   $row = mysqli_fetch_array($res);
 
   $income = $row['income'] / 1000;
