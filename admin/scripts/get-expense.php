091738dd5b2f9432026fcf7aa73f285e7ac8712b
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-13 13:44:34 +0500

    fixed expense issue

---
+++ admin/scripts/get-expense.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+  if (isset($_SESSION['user_id'])) {
+
+
+  $res = mysqli_query($appconnect, "SELECT * FROM `expenses` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
+
+  if ((mysqli_num_rows($res))>0) {
+    while ($row = mysqli_fetch_array($res)) {
+      $expense = $row['expense'];
+      $expenses +=$expense;
+    }
+    $expenses = $expenses / 1000;
+  }
+
+} else {
+      // Redirecting to for further redirection
+      header('location: /');
+    }
+?>
--- app-includes/cards/expense.php
+++ app-includes/cards/expense.php
@@ -1,20 +1,27 @@
-<?php  ?>
-<div class="card o-hidden h-100">
-  <div class="card-header bg-info text-white">
-    <h1>Expense</h1>
-  </div>
-  <div class="card-body bg-info text-white">
-    <div class="card-body-icon text-white">
-      <i class="fa fa-fw fa-rocket"></i>
+<?php
+
+  include (ABSPATH.'/admin/scripts/get-expense.php');
+
+?>
+  <div class="card o-hidden h-100">
+    <div class="card-header bg-info text-white">
+      <h1>Expense</h1>
     </div>
-    <div class="card-text text-center">
-      <span class="display-3"><strong>10.7</strong>k</span>
+    <div class="card-body bg-info text-white">
+      <div class="card-body-icon">
+        <i class="fa fa-fw fa-money text-white"></i>
+      </div>
+      <div class="card-text text-center">
+        <span class="display-3"><strong><?php printf("%.1f", $expenses); ?></strong>k</span>
+        <!--
+        <small class="text-danger"> <strong>6%<i class="fa fa-arrow-down"></i> </strong></small>
+        -->
+      </div>
     </div>
+    <a class="card-footer bg-info text-white clearfix small z-1" href="revenue.php">
+      <span class="float-left">View Details</span>
+      <span class="float-right">
+        <i class="fa fa-angle-right"></i>
+      </span>
+    </a>
   </div>
-  <a class="card-footer clearfix small z-1 bg-info text-white" href="#">
-    <span class="float-left">View Details</span>
-    <span class="float-right">
-      <i class="fa fa-angle-right"></i>
-    </span>
-  </a>
-</div>
