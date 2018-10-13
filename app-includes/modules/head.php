15ef7ccc89303c7514f01ebea25423b52b1651b7
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-13 13:51:32 +0500

    fixes

--- app-includes/modules/head.php
+++ app-includes/modules/head.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="<?php echo $PAGE_DESC; ?>">
   <meta name="author" content="<?php echo $PAGE_AUTHOR ?>">
-  <title>RC-POS - <?php echo $PAGE_TITLE; ?></title>
+  <title>Acix Sales - <?php echo $PAGE_TITLE; ?></title>
   <link href="<?php echo APPINC; ?>/css/bootstrap.css" rel="stylesheet">
   <link href="<?php echo APPINC; ?>/css/font-awesome.css" rel="stylesheet" type="text/css">
   <link href="<?php echo APPINC; ?>/css/dataTables.bootstrap4.css" rel="stylesheet">
