9b7178b0d0a753949652395c0c5b379f453829d0
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-13 08:56:27 +0500

    fixed redirect loop

--- app-includes/modules/footer-scripts.php
+++ app-includes/modules/footer-scripts.php
@@ -8,12 +8,12 @@
     <script src="<?php echo APPINC; ?>/js/rc-pos.min.js"></script>
     <script src="<?php echo APPINC; ?>/js/datatables-demo.js"></script>
     <?php
+    // Temporary fix for Redirecting error
     if ($PAGE_TITLE == "Login") {
       // code...
     }else {
       require (ABSPATH.'/admin/scripts/area-chart.php');
       require (ABSPATH.'/admin/scripts/bar-chart.php');
       require (ABSPATH.'/admin/scripts/pie-chart.php');
-
     }
     ?>
