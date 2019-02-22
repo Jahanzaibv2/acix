de73a86a3dec397cea046fe22c4efcff44476e80
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:16:59 +0500

    .

--- admin/scripts/get-top-sellings.php
+++ admin/scripts/get-top-sellings.php
@@ -6,20 +6,19 @@ if (!defined('ABSPATH'))
 
 
 
-session_start();
+  session_start();
 
   if (isset($_SESSION['user_id'])) {
+    $res = mysqli_query($appconnect, "SELECT `product_id`, COUNT(*) AS `count` FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear' GROUP BY `product_id`");
+    $row = mysqli_fetch_array($res);
 
-  $res = mysqli_query($appconnect, "SELECT `product_id`, COUNT(*) AS `count` FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear' GROUP BY `product_id`");
-  $row = mysqli_fetch_array($res);
+    $soldItems = $row['count'];
+    $topSellingProductID = $row['product_id'];
 
-  $soldItems = $row['count'];
-  $topSellingProductID = $row['product_id'];
+    $res = mysqli_query($appconnect, "SELECT * FROM `products` WHERE id='$topSellingProductID'");
+    $row = mysqli_fetch_array($res);
 
-  $res = mysqli_query($appconnect, "SELECT * FROM `products` WHERE id='$topSellingProductID'");
-  $row = mysqli_fetch_array($res);
-
-  $topSellingProduct = $row['name'];
+    $topSellingProduct = $row['name'];
   } else {
     // Redirecting to for further redirection
     header('location: /');
