2b08b38a3d175727e5669e5e254397a17ea7f40c
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:13:04 +0500

    .

--- admin/scripts/get-sales-last-month.php
+++ admin/scripts/get-sales-last-month.php
@@ -9,19 +9,15 @@
   session_start();
 
   if (isset($_SESSION['user_id'])) {
-
-
-  $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$lastMonth' AND `year`='$thisYear'");
-  $salesCount = mysqli_num_rows($res);
-
-  if ((mysqli_num_rows($res))>0) {
-    while ($row = mysqli_fetch_array($res)) {
-      $saleProfit = $row['profit'];
-      $grossProfit +=$saleProfit;
+    $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$lastMonth' AND `year`='$thisYear'");
+    $salesCount = mysqli_num_rows($res);
+    if ((mysqli_num_rows($res))>0) {
+      while ($row = mysqli_fetch_array($res)) {
+        $saleProfit = $row['profit'];
+        $grossProfit +=$saleProfit;
+      }
+      $grossProfit = $grossProfit / 1000;
     }
-    $grossProfit = $grossProfit / 1000;
-  }
-
   } else {
     // Redirecting to for further redirection
     header('location: /');
