5f587d40837ec56a5323df161d3716e0cfec18e0
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:19:35 +0500

    .

--- admin/scripts/pie-chart.php
+++ admin/scripts/pie-chart.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Pie Chart
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Acix
+ */
 
   // preventing direct script access
   if (!defined('ABSPATH'))
@@ -15,34 +30,33 @@
     header('location: /');
   }
 ?>
-<script type="text/javascript">
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#292b2c';
+  <script type="text/javascript">
+  // Set new default font family and font color to mimic Bootstrap's default styling
+  Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+  Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    datasets: [{
-      data: [ <?php echo $profitJanuary; ?>,
-              <?php echo $profitFebruary; ?>,
-              <?php echo $profitMarch; ?>,
-              <?php echo $profitApril; ?>,
-              <?php echo $profitMay; ?>,
-              <?php echo $profitJune; ?>,
-              <?php echo $profitJuly; ?>,
-              <?php echo $profitAugust; ?>,
-              <?php echo $profitSeptember; ?>,
-              <?php echo $profitOctober; ?>,
-              <?php echo $profitNovember; ?>,
-              <?php echo $profitDecember; ?>
-            ],
-      backgroundColor: ['#d35400', '#00a63f', '#900C3F', '#2e4053', '#1a5276', '#f4d03f','#8e44ad','#DAF7A6','#34eb13','#eb1d13','#0a0101'],
-    }],
-  },
-});
-
-</script>
+  // Pie Chart Example
+  var ctx = document.getElementById("myPieChart");
+  var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      datasets: [{
+        data: [ <?php echo $profitJanuary; ?>,
+                <?php echo $profitFebruary; ?>,
+                <?php echo $profitMarch; ?>,
+                <?php echo $profitApril; ?>,
+                <?php echo $profitMay; ?>,
+                <?php echo $profitJune; ?>,
+                <?php echo $profitJuly; ?>,
+                <?php echo $profitAugust; ?>,
+                <?php echo $profitSeptember; ?>,
+                <?php echo $profitOctober; ?>,
+                <?php echo $profitNovember; ?>,
+                <?php echo $profitDecember; ?>
+              ],
+        backgroundColor: ['#d35400', '#00a63f', '#900C3F', '#2e4053', '#1a5276', '#f4d03f','#8e44ad','#DAF7A6','#34eb13','#eb1d13','#0a0101'],
+      }],
+    },
+  });
+  </script>
