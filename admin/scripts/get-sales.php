dcbbb61b13bf94e962a9f3baf4bbe0612ea59181
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:15:03 +0500

    .

--- admin/scripts/get-sales.php
+++ admin/scripts/get-sales.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Get Sales
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Acix
+ */
 
   // preventing direct script access
   if (!defined('ABSPATH'))
