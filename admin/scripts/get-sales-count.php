49be0f6906346eedeeec567348a5f8906adec3b9
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:11:57 +0500

    .

--- admin/scripts/get-sales-count.php
+++ admin/scripts/get-sales-count.php
@@ -4,17 +4,12 @@
   if (!defined('ABSPATH'))
     exit('No direct script access allowed');
 
-
-
   session_start();
 
   if (isset($_SESSION['user_id'])) {
-
-
-  $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
-  $salesCount = mysqli_num_rows($res);
-} else {
-      // Redirecting to for further redirection
-      header('location: /');
-    }
-?>
+    $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
+    $salesCount = mysqli_num_rows($res);
+  } else {
+        // Redirecting to for further redirection
+        header('location: /');
+  }
