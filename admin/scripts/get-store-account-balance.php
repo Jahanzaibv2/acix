912a382784fb26dd70f832c79a3c22be450f5e96
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:16:26 +0500

    .

--- admin/scripts/get-store-account-balance.php
+++ admin/scripts/get-store-account-balance.php
@@ -23,16 +23,10 @@
   session_start();
 
   if (isset($_SESSION['user_id'])) {
-
-
-  $res = mysqli_query($appconnect, "SELECT * FROM `store_account` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
-
-  $row = mysqli_fetch_array($res);
-
-  $balance = $row['balance'] / 1000;
-
-} else {
-      // Redirecting to / for further redirection
-      header('location: /');
-    }
-?>
+    $res = mysqli_query($appconnect, "SELECT * FROM `store_account` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
+    $row = mysqli_fetch_array($res);
+    $balance = $row['balance'] / 1000;
+  } else {
+    // Redirecting to / for further redirection
+    header('location: /');
+  }
