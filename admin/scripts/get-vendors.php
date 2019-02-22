8c8226aa7f55dc1848ac179c97712309016ce3dc
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:17:38 +0500

    .

--- admin/scripts/get-vendors.php
+++ admin/scripts/get-vendors.php
@@ -4,7 +4,7 @@
 if (!defined('ABSPATH'))
     exit('No direct script access allowed');
 
-session_start();
+  session_start();
 
   if (isset($_SESSION['user_id'])) {
     $res = mysqli_query($appconnect, "SELECT * FROM `vendors`");
