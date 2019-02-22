195cfc14f4829028ceb6ac19a01c9c890501738d
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:10:17 +0500

    .

--- admin/scripts/get-product-types.php
+++ admin/scripts/get-product-types.php
@@ -8,7 +8,6 @@
   session_start();
 
   if (isset($_SESSION['user_id'])) {
-
     $getProductTypesQuery = mysqli_query($appconnect, "SELECT * FROM `product_types`");
   } else {
         // Redirecting to for further redirection
