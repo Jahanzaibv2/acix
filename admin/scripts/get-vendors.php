d5db66cbeba0c5d826b3bae33cf3340635f6a24d
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:17:34 +0500

    .

--- admin/scripts/get-vendors.php
+++ admin/scripts/get-vendors.php
@@ -7,9 +7,7 @@ if (!defined('ABSPATH'))
 session_start();
 
   if (isset($_SESSION['user_id'])) {
-
-  $res = mysqli_query($appconnect, "SELECT * FROM `vendors`");
-
+    $res = mysqli_query($appconnect, "SELECT * FROM `vendors`");
   } else {
     // Redirecting to for further redirection
     header('location: /');
