8205535481fd24ceda2c9d1e72e50111a5f00173
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:13:59 +0500

    .

--- admin/scripts/get-sales-this-month.php
+++ admin/scripts/get-sales-this-month.php
@@ -9,21 +9,16 @@
   session_start();
 
   if (isset($_SESSION['user_id'])) {
+    $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
+    $salesCount = mysqli_num_rows($res);
 
-
-  $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear'");
-  $salesCount = mysqli_num_rows($res);
-
-  if ((mysqli_num_rows($res))>0) {
-    while ($row = mysqli_fetch_array($res)) {
-      $saleProfit = $row['profit'];
-      $grossProfit +=$saleProfit;
+    if ((mysqli_num_rows($res))>0) {
+      while ($row = mysqli_fetch_array($res)) {
+        $saleProfit = $row['profit'];
+        $grossProfit +=$saleProfit;
+      }
+      $grossProfit = $grossProfit / 1000;
     }
-    $grossProfit = $grossProfit / 1000;
-  }
-
-
-
   } else {
     // Redirecting to for further redirection
     header('location: /');
