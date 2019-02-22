7854b6b6b45ad0672a64fadb098ad24c79fb4396
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 14:57:14 +0500

    .

--- logout.php
+++ logout.php
@@ -1,9 +1,22 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Logout Page
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Acix
+ */
 
   session_start();
 
   session_destroy();
 
   header('location: login.php');
-
-?>
