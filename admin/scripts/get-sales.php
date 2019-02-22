c2c196e51e5ec697488ebacb67899f09bff26aff
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:14:49 +0500

    .

--- admin/scripts/get-sales.php
+++ admin/scripts/get-sales.php
@@ -4,17 +4,11 @@
 if (!defined('ABSPATH'))
     exit('No direct script access allowed');
 
-
-
   session_start();
 
   if (isset($_SESSION['user_id'])) {
-
-
-  $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear' ORDER BY `id` DESC");
-
-
-} else {
-      // Redirecting to for further redirection
-      header('location: /');
+    $res = mysqli_query($appconnect, "SELECT * FROM `sales` WHERE `month`='$thisMonth' AND `year`='$thisYear' ORDER BY `id` DESC");
+  } else {
+    // Redirecting to for further redirection
+    header('location: /');
   }
