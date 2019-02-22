9367db3bdf2c40bccf9ede50ba4564bfbfff7894
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:02:26 +0500

    .

--- admin/app-config.php
+++ admin/app-config.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * App Configurations
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Acix
+ */
 
   define('HOST', 'localhost');
   define('USER', 'root');
