cdfc492fb0db7c80563f4f730ff95017c3afa5b7
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:21:58 +0500

    .

--- admin/scripts/update-vendors.php
+++ admin/scripts/update-vendors.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Update Vendors
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Acix
+ */
 
   // preventing direct script access
   if (!defined('ABSPATH'))
