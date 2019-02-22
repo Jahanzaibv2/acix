1a4005b8c9f05da8cc19a668f79ff0b3010a2113
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:18:10 +0500

    .

--- admin/scripts/getuserinfo.php
+++ admin/scripts/getuserinfo.php
@@ -1,17 +1,15 @@
 <?php
 
-// preventing direct script access
-if (!defined('ABSPATH'))
-    exit('No direct script access allowed');
+    // preventing direct script access
+    if (!defined('ABSPATH'))
+      exit('No direct script access allowed');
 
 
-        $ID = $_SESSION['user_id'];
-        $res = mysqli_query($appconnect, "SELECT * FROM `users` WHERE `id`='$ID'");
-        $row = mysqli_fetch_array($res);
+    $ID = $_SESSION['user_id'];
+    $res = mysqli_query($appconnect, "SELECT * FROM `users` WHERE `id`='$ID'");
+    $row = mysqli_fetch_array($res);
 
-        $username = $row['username'];
-        $fullName = $row['fullname'];
-        $email    = $row['email'];
-        $userlevel    = $row['userlevel'];
-
- ?>
+    $username = $row['username'];
+    $fullName = $row['fullname'];
+    $email    = $row['email'];
+    $userlevel    = $row['userlevel'];
