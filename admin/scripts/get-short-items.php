1417b8a5bdb8057cd68a88516635e53c370550bd
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:15:38 +0500

    .

--- admin/scripts/get-short-items.php
+++ admin/scripts/get-short-items.php
@@ -22,13 +22,9 @@
   session_start();
 
   if (isset($_SESSION['user_id'])) {
-
-
-  $res = mysqli_query($appconnect, "SELECT * FROM `products` WHERE `stock`<=3 ORDER BY `id` DESC");
-
-
-} else {
-      // Redirecting to for further redirection
-      header('location: /');
-    }
+    $res = mysqli_query($appconnect, "SELECT * FROM `products` WHERE `stock`<=3 ORDER BY `id` DESC");
+  } else {
+    // Redirecting to for further redirection
+    header('location: /');
+  }
 ?>
