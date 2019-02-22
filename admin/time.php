84e6c0e51f4cc4feb217b3701d77c18f86a97613
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 15:01:37 +0500

    .

--- admin/time.php
+++ admin/time.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Time Utility
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Acix
+ */
+
   $thisMonth = date('F');     // returns month's full name e.g December
   $thisYear  = date('Y');
   $lastMonth = date('F', strtotime("last month"));
