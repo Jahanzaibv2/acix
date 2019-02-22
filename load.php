5a3628de7ac1cb6861cf4ab49925c13d04b9e7ad
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-22 14:56:30 +0500

    .

--- load.php
+++ load.php
@@ -1,8 +1,9 @@
 <?php
-// Define ABSPATH as this file's directory
-if (!defined('ABSPATH')) {
-  define('ABSPATH', dirname(__FILE__).'/');
-}
+
+  // Define ABSPATH as this file's directory
+  if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__).'/');
+  }
 
   define('APPINC', 'app-includes');
 
