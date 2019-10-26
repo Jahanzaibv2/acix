92a0ae1ba38621dc4d2fdd0969d4f92cf56d0458
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:17:17 +0500

    .

--- admin/scripts/login.php
+++ admin/scripts/login.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Login
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
 
   // preventing direct script access
   if (!defined('ABSPATH'))
