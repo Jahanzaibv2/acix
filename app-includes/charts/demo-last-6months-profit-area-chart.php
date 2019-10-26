bc9ff1b7578f15ef4f1247fa6e5c05563ff3a0e0
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:25:19 +0500

    .

--- app-includes/charts/demo-last-6months-profit-area-chart.php
+++ app-includes/charts/demo-last-6months-profit-area-chart.php
@@ -1,4 +1,25 @@
-<?php  ?>
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Demo Last 6 Months Profit Area Chart
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
+
+  // preventing direct script access
+  if (!defined('ABSPATH'))
+    exit('No direct script access allowed');
+
+?>
 <!-- Last 6 Months Profit Area CHart-->
 <div class="card mb-3">
   <div class="card-header bg-primary text-white">
