be1e72de5df7acf113cdb37d6e75e1713ce1aa7e
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:26:20 +0500

    .

--- app-includes/modules/footer-scripts.php
+++ app-includes/modules/footer-scripts.php
@@ -1,4 +1,25 @@
-<?php  ?>
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Footer Scripts
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
+
+  // preventing direct script access
+  if (!defined('ABSPATH'))
+    exit('No direct script access allowed');
+
+?>
     <script src="<?php echo APPINC; ?>/js/jquery.min.js"></script>
     <script src="<?php echo APPINC; ?>/js/bootstrap.bundle.min.js"></script>
     <script src="<?php echo APPINC; ?>/js/jquery.easing.min.js"></script>
