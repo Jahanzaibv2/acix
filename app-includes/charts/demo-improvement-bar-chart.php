a491ec2953ff133ac2595424cfe293938b7e55a2
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:24:39 +0500

    .

--- app-includes/charts/demo-improvement-bar-chart.php
+++ app-includes/charts/demo-improvement-bar-chart.php
@@ -1,4 +1,25 @@
-<?php  ?>
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Demo Improvement Bar Chart
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
+
+  // preventing direct script access
+  if (!defined('ABSPATH'))
+    exit('No direct script access allowed');
+
+?>
 <div class="card mb-3">
   <div class="card-header bg-primary text-white">
     <i class="fa fa-chart-bar"></i>
