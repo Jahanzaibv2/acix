d380c0ad7344e13408ab200cd1d675335dfaada4
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:22:17 +0500

    .

--- app-includes/cards/short-items.php
+++ app-includes/cards/short-items.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Short Items
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
+
+  // preventing direct script access
+  if (!defined('ABSPATH'))
+    exit('No direct script access allowed');
 
 
   $res = mysqli_query($appconnect, "SELECT * FROM `products` WHERE `stock`<=3");
