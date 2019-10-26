701448df0dc35ca084055682743674bf98d0f42a
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:24:12 +0500

    .

--- app-includes/charts/area-chart.php
+++ app-includes/charts/area-chart.php
@@ -1,4 +1,25 @@
-<?php  ?>
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Area Chart
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
+
+  // preventing direct script access
+  if (!defined('ABSPATH'))
+    exit('No direct script access allowed');
+
+?>
         <!-- Area Chart -->
         <div class="card mb-3">
           <div class="card-header bg-primary text-white">
