248e52b4dda368b731462bf234a9cec466e40d8f
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:26:08 +0500

    .

--- app-includes/modules/breadcrumbs.php
+++ app-includes/modules/breadcrumbs.php
@@ -1,4 +1,25 @@
-<?php  ?>
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Breadcrumbs
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
+
+  // preventing direct script access
+  if (!defined('ABSPATH'))
+    exit('No direct script access allowed');
+
+?>
         <!-- Breadcrumbs-->
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
