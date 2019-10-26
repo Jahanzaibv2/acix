756ef2887cfaf2825e79fdeb0d80b1e5388c9b92
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-26 14:29:52 +0500

    .

--- app-includes/modules/scroll-to-top.php
+++ app-includes/modules/scroll-to-top.php
@@ -1,4 +1,26 @@
-<?php  ?>
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2018-2019
+ * 
+ * 
+ * Scross to Top
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <iam@ranajahanzaib.com>
+ * 
+ * @package Acix
+ */
+
+  // preventing direct script access
+  if (!defined('ABSPATH'))
+    exit('No direct script access allowed');
+
+
+?>
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
   <i class="fa fa-angle-up"></i>
